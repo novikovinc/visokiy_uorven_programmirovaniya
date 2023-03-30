c8e0d2438c9af9678f6c1fc00e69b7b97841d2b9
novikovinc <127538008+novikovinc@users.noreply.github.com>
2023-03-30 22:55:01 +0300

    Add files via upload

---
+++ dz2/dz2/dz2.cpp
@@ -0,0 +1,15 @@
+﻿#include <stdlib.h>
+#include <iostream>
+using namespace std;
+int main() {
+	int a, b;
+	int sum;
+	cout << "Введите 1-ое слагаемое: ";
+	cin >> a;
+	cout << "Введите 2-ое слагаемое: ";
+	cin >> b;
+	sum = a + b;
+	cout << "Сумма: " << sum << endl;
+	system("PAUSE");
+	return 0;
+}
