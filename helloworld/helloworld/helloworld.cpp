0142b21ff61f5714aec9fe37d5b8780d11b05f84
novikovinc <127538008+novikovinc@users.noreply.github.com>
2023-03-30 22:54:21 +0300

    Add files via upload

---
+++ helloworld/helloworld/helloworld.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+int main() {
+	// комментарий
+	cout << "Привет, Мир! \n";
+	cout << "Я студент кафедры системного анализа и информатизации!!! \n";
+	system("PAUSE");
+	return 0;
+}
